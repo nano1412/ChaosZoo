c3c9204568fbc7edaaddda660a3f9fe8e707ca47
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2024-08-15 00:51:56 +0700

    Add : Script Playe01Move / Player01Action

---
+++ Assets/Real/Player01Action.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01Action : MonoBehaviour
+{
+    public float Jumpspeed = 0.01f;
+    public GameObject Player01;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void JumpUp()
+    {
+        //Player01.transform.Translate(0, Jumpspeed, 0);
+    }
+}
--- Assets/Real/Player01Move.cs
+++ Assets/Real/Player01Move.cs
@@ -4,44 +4,153 @@ using UnityEngine;
 
 public class Player01Move : MonoBehaviour
 {
+    public float walkSpeed = 0.0015f;
+    public float JumpSpeed = 0.02f;
+    public GameObject player01;
+    public GameObject opponent;
+    public Vector3 oppPosition;
 
-    private Animator anim;
+    private bool IsJumping = false;
+    private Rigidbody rb;
+    private Animator anim; 
+    private AnimatorStateInfo Player01Layer0;
+    private bool canWalkleft = true;
+    private bool canWalkright = true;
+    private bool FaceingLeft = false;
+    private bool FaceingRight = true;
+
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
-
 
     void Update()
     {
-        //Horizontal walking left and right
-        if(Input.GetAxis("Horizontal") > 0)
+        Player01Layer0 = anim.GetCurrentAnimatorStateInfo(0);
+
+        //Vector3 ScreenBounds = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        /*if(ScreenBounds.x > Screen.width)
         {
-            anim.SetBool("Forward", true);
+            canWalkright = false;
         }
-        if(Input.GetAxis("Horizontal") < 0)
+        if(ScreenBounds.x < 0)
         {
-            anim.SetBool("Backward", true);
+            canWalkleft = false;
         }
-        if(Input.GetAxis("Horizontal") == 0)
+        else
         {
-            anim.SetBool("Forward", false);
-            anim.SetBool("Backward", false);
+            canWalkleft = true;
+            canWalkright = true;
+        }*/ 
+
+        //Get the oppenet position
+        oppPosition = opponent.transform.position;
+        //Facing left or right
+         if (oppPosition.x > player01.transform.position.x && !FaceingRight)
+        {
+            StartCoroutine(FaceRight());
+        }
+        else if (oppPosition.x < player01.transform.position.x && !FaceingLeft)
+        {
+            StartCoroutine(FaceLeft());
         }
 
-        //Vertical Jump and Crouch
-        if(Input.GetAxis("Vertical") > 0)
+        // Horizontal walking left and right
+        if (Player01Layer0.IsTag("Motion"))
         {
-            anim.SetTrigger("Jump");
+            if (Input.GetAxis("Horizontal") > 0 && canWalkright)
+            {
+                anim.SetBool("Forward", true);
+                transform.Translate(walkSpeed, 0, 0);
+            }
+            else if (Input.GetAxis("Horizontal") < 0 && canWalkleft)
+            {
+                anim.SetBool("Backward", true);
+                transform.Translate(-walkSpeed, 0, 0);
+            }
+            else
+            {
+                anim.SetBool("Forward", false);
+                anim.SetBool("Backward", false);
+            }
         }
-        if(Input.GetAxis("Vertical") < 0)
+
+        // Vertical Jump and Crouch
+        if (Input.GetAxis("Vertical") > 0 && !IsJumping)
+        {
+            IsJumping = true;
+            anim.SetTrigger("Jump");
+            rb.AddForce(Vector3.up * JumpSpeed, ForceMode.Impulse);
+            StartCoroutine(JumpPause());
+        }
+        else if (Input.GetAxis("Vertical") < 0)
         {
             anim.SetBool("Crouch", true);
         }
-        if(Input.GetAxis("Vertical") == 0)
+        else
         {
             anim.SetBool("Crouch", false);
         }
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "WallLeft")
+        {
+            canWalkleft = false;
+        }
+        if (collision.gameObject.tag == "WallRight")
+        {
+            canWalkright = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "WallLeft")
+        {
+            canWalkleft = true;
+        }
+        if (collision.gameObject.tag == "WallRight")
+        {
+            canWalkright = true;
+        }
+    }
+
+    IEnumerator JumpPause()
+    {
+        yield return new WaitForSeconds(1.0f);
+        IsJumping = false;
+    }
+
+    IEnumerator FaceLeft()
+    {
+        if (!FaceingLeft)
+        {
+            FaceingLeft = true;
+            FaceingRight = false;
+            yield return new WaitForSeconds(0.15f);
+            // Flip the character by inverting the scale on the X axis
+            Vector3 newScale = transform.localScale;
+            newScale.x = Mathf.Abs(newScale.x); // Ensure it's positive before multiplying
+            newScale.x *= -1;  // Invert the X scale to face left
+            transform.localScale = newScale;
+        }
+    }
+
+    IEnumerator FaceRight()
+    {
+        if (!FaceingRight)
+        {
+            FaceingRight = true;
+            FaceingLeft = false;
+            yield return new WaitForSeconds(0.15f);
+            // Flip the character by inverting the scale on the X axis
+            Vector3 newScale = transform.localScale;
+            newScale.x = Mathf.Abs(newScale.x); // Ensure it's positive before multiplying
+            transform.localScale = newScale;
+        }
     }
 }
