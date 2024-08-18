81b330d2ad1408e53e204aa6429298136d06a698
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2024-08-18 16:40:50 +0700

    Add : Challenge02 and Challenge03 and Clear Script table goto Scene

--- Assets/Real/SceneChanger.cs
+++ Assets/Real/SceneChanger.cs
@@ -5,8 +5,11 @@ public class SceneChangerButton : MonoBehaviour
 {
     public string sceneToLoad = "01";
     public string sceneToLoadInChallenge01 = "Challenge01";
-     public string sceneToLoadInChallenge02 = "Challenge02";
-      public string sceneToLoadInChallenge03 = "Challenge03";
+    public string sceneToLoadInChallenge02 = "Challenge02";
+    public string sceneToLoadInChallenge03 = "Challenge03";
+    public ChalllengeScripttable challlengeScripttable01;
+    public ChalllengeScripttable challlengeScripttable02;
+    public ChalllengeScripttable challlengeScripttable03;
     public GameObject selectlevel;
 
     public void ChangeTraining()
@@ -28,14 +31,28 @@ public class SceneChangerButton : MonoBehaviour
     public void SelectChallenge01()
     {
         SceneManager.LoadScene(sceneToLoadInChallenge01);
+        ClearChallengeData(challlengeScripttable01);
+
     }
     public void SelectChallenge02()
     {
         SceneManager.LoadScene(sceneToLoadInChallenge02);
+        ClearChallengeData(challlengeScripttable02);
+
     }
     public void SelectChallenge03()
     {
         SceneManager.LoadScene(sceneToLoadInChallenge03);
+        ClearChallengeData(challlengeScripttable03);
+
     }
 
+    private void ClearChallengeData(ChalllengeScripttable challengeData)
+    {
+        // Reset CurrentRound to 0
+        challengeData.CurrentRound = 0;
+
+        // Clear the boolList
+        challengeData.boolList.Clear(); // ต้องใช้วงเล็บ () เพื่อเรียกใช้เมธอด
+    }
 }
