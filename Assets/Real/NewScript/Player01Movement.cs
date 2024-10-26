bbc38b3f8bb88506393108899d572bc13f3cfbe7
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2024-10-26 15:31:15 +0700

    EDIT: VerticalAxis variable of Player01 to negative. OPTIMIZE: WalkThreshold of Player01

--- Assets/Real/NewScript/Player01Movement.cs
+++ Assets/Real/NewScript/Player01Movement.cs
@@ -59,7 +59,7 @@ public class Player01Movement : MonoBehaviour
         {
             horizontalInput = "LeftAnalogX1";
             verticalInput = "LeftAnalogY1";
-            walkThreshold = 0.99f;
+            walkThreshold = 0.4f;
             verticalThreshold = 0.4f;
         }
 
@@ -71,7 +71,7 @@ public class Player01Movement : MonoBehaviour
     public void HandleCrouch()
     {
         float verticalAxis = Input.GetAxis(verticalInput);
-        if(verticalAxis < -verticalThreshold && IsGrounded())
+        if(-verticalAxis < -verticalThreshold && IsGrounded())
         {
             animationCouch = true;
             anim.SetBool("Crouch", true);
@@ -111,7 +111,7 @@ public class Player01Movement : MonoBehaviour
     {
         float verticalAxis = Input.GetAxis(verticalInput);
 
-        if(verticalAxis > verticalThreshold && !IsJumping && !animationCouch)
+        if(-verticalAxis > verticalThreshold && !IsJumping && !animationCouch)
         {
             IsJumping = true;
             anim.SetTrigger("Jump");
