9a12d9a2a1ffbf0f3683a8f19abb4093dba7f415
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2024-11-02 14:59:34 +0700

    Add : AttackBox player 01 and 02 Shark

---
+++ Assets/Real/NewScript/Player01Health.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01Health : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "5P_AttackBox" || col.tag == "5K_AttackBox" || col.tag == "5S_AttackBox" || col.tag == "5HS_AttackBox" ||
+           col.tag == "2P_AttackBox" || col.tag == "2K_AttackBox" || col.tag == "2S_AttackBox" || col.tag == "2HS_AttackBox" ||
+           col.tag == "6P_AttackBox" || col.tag == "6K_AttackBox" || col.tag == "6S_AttackBox" || col.tag == "6HS_AttackBox")
+        {
+            //anim.SetTrigger("Hurt");
+            Debug.Log("Attack");
+        }
+    }
+
+}
