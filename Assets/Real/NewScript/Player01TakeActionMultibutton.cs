9df6ef27b7a0b25b88f915171d53e2a7f0065efa
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2024-11-25 23:45:50 +0700

    Add : DashForward and DashBackward_Capybara_player01

---
+++ Assets/Real/NewScript/Player01TakeActionMultibutton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player01TakeActionMultibutton : MonoBehaviour
+{
+    public float defaultActionCooldown = 0.5f;
+    public string nameCharacter;
+    public GameObject player01;
+    public Player01Movement player01Movement;
+    public Player01Health player01Health;
+    public SelectController selectController;
+
+}
