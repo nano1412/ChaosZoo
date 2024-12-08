282225a84524767a39553780ada486e3dae1dfb9
LAPTOP-VBON3RIL\Napat Kemachaiwead <napat.kemachaiwead@mail.kmutt.ac.th>
2024-12-08 17:09:23 +0700

    Add : Mainmenu Select challenge button

--- Assets/Real/SceneChanger.cs
+++ Assets/Real/SceneChanger.cs
@@ -10,8 +10,14 @@ public class SceneChangerButton : MonoBehaviour
     public GameObject selectChallenge;
     public GameObject trainingModeButton;
     public GameObject challengeModeButton;
+    public GameObject selectChallengeShark;
+    public GameObject selectChallengeCapybara;
+    public GameObject selectChallengeKen;
+    public GameObject selectChallengePengang;
     public GameObject camera01;
     public GameObject camera02;
+    public GameObject cameraSharkChallenge;
+    public GameObject cameraCapybaraChallenge;
 
     void Start()
     {
@@ -52,6 +58,57 @@ public class SceneChangerButton : MonoBehaviour
         challengeModeButton.SetActive(true);
     }
 
+    public void SelectChallengeShark()
+    {
+        cameraSharkChallenge.SetActive(true);
+        cameraCapybaraChallenge.SetActive(false);
+        selectChallengeShark.SetActive(true);
+        selectChallengeCapybara.SetActive(false);
+        selectChallengeKen.SetActive(false);
+        selectChallengePengang.SetActive(false);
+        selectChallenge.SetActive(false);
+    }
+
+    public void SelectChallengeCapybara()
+    {
+        cameraSharkChallenge.SetActive(false);
+        cameraCapybaraChallenge.SetActive(true);
+        selectChallengeShark.SetActive(false);
+        selectChallengeCapybara.SetActive(true);
+        selectChallengeKen.SetActive(false);
+        selectChallengePengang.SetActive(false);
+        selectChallenge.SetActive(false);
+    }
+
+    public void SelectChallengeKen()
+    {
+        selectChallengeShark.SetActive(false);
+        selectChallengeCapybara.SetActive(false);
+        selectChallengeKen.SetActive(true);
+        selectChallengePengang.SetActive(false);
+        selectChallenge.SetActive(false);
+    }
+
+    public void SelectChallengePengang()
+    {
+        selectChallengeShark.SetActive(false);
+        selectChallengeCapybara.SetActive(false);
+        selectChallengeKen.SetActive(false);
+        selectChallengePengang.SetActive(true);
+        selectChallenge.SetActive(false);
+    }
+
+    public void ExitChallengeSelectNumber()
+    {
+        cameraSharkChallenge.SetActive(false);
+        cameraCapybaraChallenge.SetActive(false);
+        selectChallengeShark.SetActive(false);
+        selectChallengeCapybara.SetActive(false);
+        selectChallengeKen.SetActive(false);
+        selectChallengePengang.SetActive(false);
+        selectChallenge.SetActive(true);
+    }
+
     public void TrainingRoomShark()
     {
         SceneManager.LoadScene(sceneToLoad[3]);
@@ -71,17 +128,43 @@ public class SceneChangerButton : MonoBehaviour
     {
         SceneManager.LoadScene(sceneToLoad[2]);
     }
-
-    public void ChallengeShark()
+    
+    public void ChallengeShark01()
     {
-        SceneManager.LoadScene(sceneToLoad[7]);
+        SceneManager.LoadScene(sceneToLoad[16]);
         ClearChallengeData(challengeScriptable[12]);
         ClearChallengeData(challengeScriptable[13]);
         ClearChallengeData(challengeScriptable[14]);
         ClearChallengeData(challengeScriptable[15]);
     }
 
-    public void ChallengeCapybara()
+    public void ChallengeShark02()
+    {
+        SceneManager.LoadScene(sceneToLoad[17]);
+        ClearChallengeData(challengeScriptable[12]);
+        ClearChallengeData(challengeScriptable[13]);
+        ClearChallengeData(challengeScriptable[14]);
+        ClearChallengeData(challengeScriptable[15]);
+    }
+    public void ChallengeShark03()
+    {
+        SceneManager.LoadScene(sceneToLoad[18]);
+        ClearChallengeData(challengeScriptable[12]);
+        ClearChallengeData(challengeScriptable[13]);
+        ClearChallengeData(challengeScriptable[14]);
+        ClearChallengeData(challengeScriptable[15]);
+    }
+
+    public void ChallengeSharkOD()
+    {
+        SceneManager.LoadScene(sceneToLoad[19]);
+        ClearChallengeData(challengeScriptable[12]);
+        ClearChallengeData(challengeScriptable[13]);
+        ClearChallengeData(challengeScriptable[14]);
+        ClearChallengeData(challengeScriptable[15]);
+    }
+
+    public void ChallengeCapybara01()
     {
         SceneManager.LoadScene(sceneToLoad[4]);
         ClearChallengeData(challengeScriptable[0]);
@@ -90,18 +173,96 @@ public class SceneChangerButton : MonoBehaviour
         ClearChallengeData(challengeScriptable[3]);
     }
 
-    public void ChallengeKen()
+    public void ChallengeCapybara02()
     {
         SceneManager.LoadScene(sceneToLoad[5]);
+        ClearChallengeData(challengeScriptable[0]);
+        ClearChallengeData(challengeScriptable[1]);
+        ClearChallengeData(challengeScriptable[2]);
+        ClearChallengeData(challengeScriptable[3]);
+    }
+
+    public void ChallengeCapybara03()
+    {
+        SceneManager.LoadScene(sceneToLoad[6]);
+        ClearChallengeData(challengeScriptable[0]);
+        ClearChallengeData(challengeScriptable[1]);
+        ClearChallengeData(challengeScriptable[2]);
+        ClearChallengeData(challengeScriptable[3]);
+    }
+
+    public void ChallengeCapybaraOD()
+    {
+        SceneManager.LoadScene(sceneToLoad[7]);
+        ClearChallengeData(challengeScriptable[0]);
+        ClearChallengeData(challengeScriptable[1]);
+        ClearChallengeData(challengeScriptable[2]);
+        ClearChallengeData(challengeScriptable[3]);
+    }
+
+    public void ChallengeKen01()
+    {
+        SceneManager.LoadScene(sceneToLoad[8]);
         ClearChallengeData(challengeScriptable[4]);
         ClearChallengeData(challengeScriptable[5]);
         ClearChallengeData(challengeScriptable[6]);
         ClearChallengeData(challengeScriptable[7]);
     }
 
-    public void ChallengePengang()
+    public void ChallengeKen02()
     {
-        SceneManager.LoadScene(sceneToLoad[6]);
+        SceneManager.LoadScene(sceneToLoad[9]);
+        ClearChallengeData(challengeScriptable[4]);
+        ClearChallengeData(challengeScriptable[5]);
+        ClearChallengeData(challengeScriptable[6]);
+        ClearChallengeData(challengeScriptable[7]);
+    }
+
+    public void ChallengeKen03()
+    {
+        SceneManager.LoadScene(sceneToLoad[10]);
+        ClearChallengeData(challengeScriptable[4]);
+        ClearChallengeData(challengeScriptable[5]);
+        ClearChallengeData(challengeScriptable[6]);
+        ClearChallengeData(challengeScriptable[7]);
+    }
+
+    public void ChallengeKenOD()
+    {
+        SceneManager.LoadScene(sceneToLoad[11]);
+        ClearChallengeData(challengeScriptable[4]);
+        ClearChallengeData(challengeScriptable[5]);
+        ClearChallengeData(challengeScriptable[6]);
+        ClearChallengeData(challengeScriptable[7]);
+    }
+
+    public void ChallengePengang01()
+    {
+        SceneManager.LoadScene(sceneToLoad[12]);
+        ClearChallengeData(challengeScriptable[8]);
+        ClearChallengeData(challengeScriptable[9]);
+        ClearChallengeData(challengeScriptable[10]);
+        ClearChallengeData(challengeScriptable[11]);
+    }
+    public void ChallengePengang02()
+    {
+        SceneManager.LoadScene(sceneToLoad[13]);
+        ClearChallengeData(challengeScriptable[8]);
+        ClearChallengeData(challengeScriptable[9]);
+        ClearChallengeData(challengeScriptable[10]);
+        ClearChallengeData(challengeScriptable[11]);
+    }
+    public void ChallengePengang03()
+    {
+        SceneManager.LoadScene(sceneToLoad[14]);
+        ClearChallengeData(challengeScriptable[8]);
+        ClearChallengeData(challengeScriptable[9]);
+        ClearChallengeData(challengeScriptable[10]);
+        ClearChallengeData(challengeScriptable[11]);
+    }
+    public void ChallengePengangOD()
+    {
+        SceneManager.LoadScene(sceneToLoad[15]);
         ClearChallengeData(challengeScriptable[8]);
         ClearChallengeData(challengeScriptable[9]);
         ClearChallengeData(challengeScriptable[10]);
